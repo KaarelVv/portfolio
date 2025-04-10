c65023c98c84c690519f9b2e960d148e675db62f
Kaarel Viilvere <viilvere.k@gmail.com>
2025-04-10 17:37:09 +0300

    refactor2

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,21 +1,28 @@
 
 export const projects = [
-    {
-      id: 1,
-      title: 'First Cool App',
-      description: 'Description here...',
-      image: '/assets/images/app1.png',
-      git:'myGitUrl',
-      url:'myProjectURL'
-    },
-    {
-        id: 2,
-        title: 'Second Cool App',
-        description: 'Description here...',
-        image: '/assets/images/app1.png',
-        git:'myGitUrl',
-      url:'myProjectURL'
-      }
-   
-  ];
-  
+  {
+    id: 1,
+    title: 'First Cool App',
+    description: 'Description here...',
+    image: '/assets/images/app1.png',
+    git: 'myGitUrl',
+    url: 'https://vso24viilvere.ita.voco.ee/veebiarendus/portfolio/'
+  },
+  {
+    id: 2,
+    title: 'Second Cool App',
+    description: 'Description here...',
+    image: '/assets/images/app1.png',
+    git: 'myGitUrl',
+    url: 'https://vso24viilvere.ita.voco.ee'
+  },
+  {
+    id: 3,
+    title: 'Third Cool App',
+    description: 'Description here...',
+    image: '/assets/images/app1.png',
+    git: 'myGitUrl',
+    url: 'https://hindamine.ita.voco.ee'
+  }
+
+];
